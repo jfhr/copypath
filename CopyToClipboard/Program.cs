23c9e20d83a5155314df7536652ea300d4e3e039
Jakob Fahr <1411423-jfahr@users.noreply.gitlab.com>
2019-01-31 00:00:54 +0100

    Initial commit

---
+++ CopyToClipboard/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace CopyToClipboard
+{
+    class Program
+    {
+        const string IconPath = @"%SystenRoot%\system32\shell32.dll,605";
+        const string CommandLabel = "Copy file path";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                // install as explorer shortcut
+                try
+                {
+                    InstallExplorerShortcut();
+                }
+                catch (ObjectDisposedException e)
+                {
+                    MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (SecurityException e)
+                {
+                    MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                // copy first argument to clipboard
+                // other arguments will be ignored
+                string resPath = FormatPath(args[0]);
+                Clipboard.SetData(DataFormats.UnicodeText, resPath);
+            }
+        }
+
+        static string FormatPath(string path)
+        {
+            // replace '\' with '/' so the user does not have to escape the path
+            return path.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+        }
+
+        static void InstallExplorerShortcut()
+        {
+            RegistryKey root = Registry.CurrentUser.OpenSubKey("Software", true).OpenSubKey("Classes", true);
+            RegistryKey directory = root.OpenSubKey("Directory", true).OpenSubKey("shell", true);
+            RegistryKey directorySidebar = root.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("shell", true);
+            RegistryKey file = root.OpenSubKey("*", true).OpenSubKey("shell", true);
+            RegistryKey drive = root.OpenSubKey("Drive", true).OpenSubKey("shell", true);
+
+            InstallExplorerShortcut(directory);
+            InstallExplorerShortcut(directorySidebar);
+            InstallExplorerShortcut(file);
+            InstallExplorerShortcut(drive);
+        }
+        static void InstallExplorerShortcut(RegistryKey root)
+        {
+            RegistryKey location = root.CreateSubKey("CopyPath");
+            location.SetValue(null, CommandLabel);
+            location.SetValue("Icon", IconPath);
+            RegistryKey command = location.CreateSubKey("command");
+            command.SetValue(null, $"{Assembly.GetExecutingAssembly().Location} \"%1\"");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# CopyPath
+
+Tiny .NET program that lets you copy the full path of a file/folder from within Windows Explorer.
+
+## Build Instructions
+
+* If you haven't already, install [Visual Studio 2017](https://visualstudio.microsoft.com/vs/).  When installing, make sure you select the required .NET framework tools (.NET Version 4.7.2 is recommended, but anything from 3.0 should work)
+* Open file CopyToClipboard.sln
+* Select Configuration "Release"
+* Select Build > Build Solution
+* The executable should be in bin/release/CopyToClipboard.exe
+
+## Usage
+
+* Run the executable once without any arguments in order to install the Windows Explorer Shortcuts.
+* Open Windows Explorer
+* Right-click any file or folder
+* You should see an option like this: 
+
+![Image of the Windows Explorer context menu with an option saying 'Copy file path'](Screenshot.png)
+
+* Selecting the option should copy the path into your clipboard. The path will use forward-slashes instead of backward-slashes, so you can use it in a string without the need to escape it.
